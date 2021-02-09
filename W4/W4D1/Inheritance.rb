272e95b928ee7223e5996cad40f995591d07ef98
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-02-09 15:09:22 -0800

    work

--- W4/W4D1/Inheritance.rb
+++ W4/W4D1/Inheritance.rb
@@ -1,39 +1,45 @@
 class Employee
-    attr_reader :name, :title , :salary , :boss
+  attr_reader :name, :title, :salary, :boss
 
-    def initialize(name,title,salary,boss)
-        @name = name
-        @title=title
-        @salary=salary
-        @boss=boss
-    end
+  def initialize(name, title, salary, boss = nil)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
 
-    def bonus(multiplier)
-       @salary * multiplier 
-    end
-
+  def bonus(multiplier)
+    @salary * multiplier
+  end
 end
 
 class Manager < Employee
-    attr_reader :employees
+  attr_reader :employees
 
-    def initialize(name,title,salary,boss)
-        @employees = []
+  def initialize(name, title, salary, boss = nil)
+    @employees = []
+  end
 
+  def add_employee(subordinate)
+      @employees << subordinate
+  end
+
+  def bonus(multiplier)
+    sum = 0
+    @employees.each do |employee|
+      if employee.is_a?(Manager)
+        sum += self.bonus(employee)
+      else
+        sum += (@salary * multiplier)
+      end
     end
-
-    def bonus(multiplier)
-        sum = 0
-        @employees.each do |employee|
-            if employee.is_a?(Manager)
-                sum += self.bonus(employee)
-            else
-           sum += employee.salary 
-            end
-        end
-        sum
-    end
+    sum
+  end
 end
 
-employee1=Employee.new('Dave',"director",'5000','John')
-manager1=Manger.new('john', 'manager', '10000')
+manager1 = Manager.new("john", "manager", 10000)
+employee1 = Employee.new("Dave", "director", 5000, manager1)
+manager1.add_employee(employee1)
+p manager1.employees
+p employee1.bonus(5)
+p manager1.bonus(10)
