32f25b0e10870374f4d3a8a88e4dfcdd1a3012fc
Kevin <kevinsebastianmathews@gmail.com>
2021-02-12 17:03:09 -0800

    done

--- W4/W4D1/two_sum.rb
+++ W4/W4D1/two_sum.rb
@@ -19,5 +19,38 @@ end
 
 def better_two_sum?(arr, target)
   arr = arr.sort                        #nlogn
-  arr.each_with_index 
+  arr.each_with_index do |ele , i |
+    newarr=arr[0...i] + arr[i+1..-1]
+    return true if newarr.bsearch_index{|ele2| target <=> ele2+ele }
+  end
+  false
 end
+
+#nlogn
+
+
+# p better_two_sum?([1, 4, 5, 6], 2)
+
+def best_two_sum?(arr, target)
+
+  hash={}
+  newarr= []  
+  arr.each do |ele| 
+    hash[ele] = target-ele
+
+  end
+  hash.each do |ele,val| 
+   newarr <<  ele if hash.has_value?(ele)
+
+  end
+
+  return true if newarr.length > 1 
+  else
+  return false
+
+end
+
+#0(n)
+
+
+#p best_two_sum?([1, 4, 5, 6],9)
